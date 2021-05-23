9dd097f2e5de1483332f7948605d6951ea4ee9b5
timofeykl <84674136+timofeykl@users.noreply.github.com>
2021-05-23 19:11:55 +0300

    Main_code

--- 3d_math_pot_lib.py
+++ 3d_math_pot_lib.py
@@ -5,73 +5,60 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-
+#connection to Oracle DB stuff
 dsn = """(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)
-(HOST=rnb-vmdwh-dbpr04.rci.renault.ru)(PORT=1521))
-(ADDRESS=(PROTOCOL=TCP)(HOST=rnb-vmdwh-dbpr03.rci.renault.ru)(PORT=1521))
-(LOAD_BALANCE= yes))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=DWHPR_DG)))
+(HOST=!!!host_address)(PORT=!!!port))
+(ADDRESS=(PROTOCOL=TCP)(HOST=!!!host_address)(PORT=1521))
+(LOAD_BALANCE= yes))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=!!!SERVICE_NAME)))
 """
-connection = cx.connect("iz01340", "iz01340", dsn)
+connection = cx.connect("USER", "PASSWORD", dsn)
 
 print('fine')
 query = """
-select
-brand
-, PRODUCT_NAME
-, GENERATION
-, GOS_PROGRAM_STATUS
-, BRAND
-, CAR_PRICE
-, INITIAL_PAYMENT
-, TOTAL_FINANCE_AMOUNT
-, CPI_PREMIUM_CONTRACT
-, CASCO_PREMIUM
-, M_OF_FINANCING
-, Y_OF_FINANCING
-, MODEL_CODE
-, INIT_CONTRIB_INT_RATE
-, FIO_APPLICANT
-, LOAN_FOR_CAR_SUM
-, dealer_service_sum
-, GOS_PROGRAM_NAME
-, GOS_PROGRAM_AMOUNT
-, GOS_PROGRAM_SUBSIDY
-, CREDIT_ISSUE_DATE
-from dwh.v_indicator_fpa
-WHERE CREDIT_ISSUE_DATE>=DATE'2020-08-01' AND CREDIT_ISSUE_DATE<DATE'2021-02-01'
-AND BRAND='Renault' and NEW_USED='NEW'
+!!!easy select over here
            """
 
 df = pd.read_sql(query, con=connection)
 print(1)
 
-
-model = df['MODEL_CODE'].str.split('_').str[0]
+#extracting everything before underscore 
+model = df['AGG_COL'].str.split('_').str[0]
+#trasforming it to uppercase
 model = model.str.upper()
 
+#changing column from previous perspective
 df['MODEL'] = model
 
-df['M_OF_FINANCING'] = df['M_OF_FINANCING'].replace({'08':1, '09':2, '10':3, '11':4, '12':5, '01':6})
+#hand made stuff 
+#under analysis we are to consider earliest month as first one
+df['MONTH'] = df['MONTH'].replace({'08':1, '09':2, '10':3, '11':4, '12':5, '01':6})
 
-df["CAR_PRICE"] = df["CAR_PRICE"].div(1000).round(2)
+#k-ing and round-ing, baby
+df["CPRICE"] = df["CAR_PRICE"].div(1000).round(2)
+
+#check it out, baby
 # print(df)
 # group_m_model = df.groupby(["MODEL", "M_OF_FINANCING"])["CAR_PRICE"].describe().unstack()
 # print(group_m_model)
 
+#grouping out stuff
 df = df.groupby(['MODEL', 'M_OF_FINANCING'])["CAR_PRICE"]\
     .agg(['count', 'mean'])
 
-
+#nowhere to run from excel
 df.to_excel('output.xlsx')
 
+#let's exercise some more with excel
 df = pd.read_excel('output.xlsx')
-
 print(df)
+
+#up to 3d we go
 fig = plt.figure()
+#password: Wolfenstein.. ...you name it
 ax = fig.add_subplot(111, projection='3d')
 
 
-
+#documentation quotes. you should be realising by now, that we can't be serious here ;)
 # For each set of style and range settings, plot n random points in the box
 # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
 for c, m, df['mean'].max, df['mean'].min in [('b', 'o', 790, 1500), ('r', '^', 1500, 2410)]:
@@ -86,7 +73,7 @@ ax.set_xlabel('M_OF_FINANCING')
 
 plt.show()
 
-
+# and another... method I think below 
 
 # td = plt.figure().gca(projection='3d')
 # td.scatter(df.index, df['count'], df["M_OF_FINANCING"])
